147e1270440cb22c7dc2bbeac935bd91d3a33f1d
alicefu <69636893+alicejfu@users.noreply.github.com>
2020-10-06 00:08:14 -0400

    function component

--- client/components/Card-copy.jsx
+++ client/components/Card-copy.jsx
@@ -1,30 +1,25 @@
-import React, { Component, useState } from "react";
-import SideA from "./SideA";
-import SideB from "./SideB";
-// import Flippy, { FrontSide, BackSide } from "react-flippy";
+import React from 'react';
+import SideA from './SideA';
+import SideB from './SideB';
 
 const Card = (props) => {
   const sideDiv = !props.cardStatus.flipped ? (
     <SideA
-      className='front'
+      className="front"
       onCardClick={props.onCardClick}
       cardStatus={props.cardStatus}
       id={props.id}
-      // onClick={() => {
-      //   console.log("isFlipped?", props.cardStatus.flipped);
-      //   props.onCardClick(props.id, props.cardStatus);
-      // }}
     />
   ) : (
     <SideB
-      id='back'
-      className='back'
+      id="back"
+      className="back"
       cardValue={props.cardValue}
       cardStatus={props.cardStatus}
     />
   );
 
-  return <div className='card'>{sideDiv}</div>;
+  return <div className="card">{sideDiv}</div>;
 };
 
 export default Card;
--- client/components/Game.jsx
+++ client/components/Game.jsx
@@ -1,45 +1,18 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React from 'react';
 import Board from './Board.jsx';
 import InfoContainer from './InfoContainer.jsx';
 import LeaderBoard from './LeaderBoard';
 import Message from './Message';
-// Board, Info Container
 
-class Game extends Component {
-  render() {
-    console.log(
-      'props.state.leaderBoard in game',
-      this.props.state.leaderBoard
-    );
-    // const names = [
-    //   'Will',
-    //   'Phill',
-    //   'Kyle',
-    //   'Sara',
-    //   'Keiran',
-    //   'Jeho',
-    //   'Wayne',
-    //   'Midori',
-    // ];
-    // const prevVal = this.props.state.previousCard.cardValue;
-    // const curVal = this.props.state.currentCard.cardValue;
-    // const firstPick = prevVal > -1 ? names[prevVal] : undefined;
-    // const secondPick = curVal > -1 ? names[curVal] : undefined;
-    // const found = firstPick === secondPick ? firstPick : undefined;
-    return (
-      <div className="Game">
-        <InfoContainer state={this.props.state} />
-        {/* <div className="special-messages">
-          <div>{firstPick}</div>
-          <div>{secondPick}</div>
-        </div> */}
-        <Message state={this.props.state} />
-        <Board state={this.props.state} onCardClick={this.props.onCardClick} />
-        <LeaderBoard leaderBoard={this.props.state.leaderBoard} />
-      </div>
-    );
-  }
-}
+const Game = (props) => {
+  return (
+    <div className="Game">
+      <InfoContainer state={props.state} />
+      <Message state={props.state} />
+      <Board state={props.state} onCardClick={props.onCardClick} />
+      <LeaderBoard leaderBoard={props.state.leaderBoard} />
+    </div>
+  );
+};
 
 export default Game;
